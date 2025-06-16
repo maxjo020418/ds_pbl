fbc4d9c4ddcd97523ca6d51037ffd44fcac03271
maxjo020418 <maxjo020418@gmail.com>
2025-06-16 12:49:54 +0900

    minor changes

---
+++ SQL/export.sql
@@ -0,0 +1,9 @@
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+
+SELECT *
+INTO OUTFILE '/var/lib/mysql-files/reviews.csv'
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+FROM library_reviews;
